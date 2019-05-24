34d86bb5141fefc9835340c9d0c3cf863b20a49b
Parker Escalette <Parkerescalette@gmail.com>
2019-05-24 03:47:28 -0700

    added small test

---
+++ test.py
@@ -0,0 +1,14 @@
+from lynx import *
+
+
+def foo():
+    print('called foo')
+
+
+if __name__ == '__main__':
+    item1 = MenuItem('empty test 1')
+    item2 = MenuItem('empty test 2')
+    subMenu1 = Menu(item1, item2)
+    item3 = MenuItem('sub menu', subMenu=subMenu1, callback=foo)
+    testMenu = Menu(item1, item2, item3)
+    testMenu.show()
